e5d2307a35ececd63c1960eb2577629ec90a13e0
gopalakrishnanv708 <gopalakrishnanv708@gmail.com>
2023-01-10 07:10:18 +0530

    Add project files.

---
+++ ReDpett/Modal/FileData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDpett.Modal
+{
+    public class FileData
+    {
+        public string Att_FileName { get; set; }
+        public string Report_Title { get; set; }
+        public string TypeOfReport { get; set; }
+    }
+}
